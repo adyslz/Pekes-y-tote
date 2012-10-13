b572691a704dbc80b097424568ab28954588492d
Adriana Lopez Zarate <adys.lz@hotmail.com>
2012-10-12 19:39:13 -0500

    Modificacion form administrador aprobar evento

--- js/admEvent.js
+++ js/admEvent.js
@@ -20,22 +20,22 @@ function aprobado(opc){
 function rechazado(opc){
 	switch(opc){
 		case 1: formNvo = document.createElement("form");
-				formNvo.id = "formrechazar"; 
+				formNvo.id = "formrechazar1"; 
   				infohackers.appendChild(formNvo); 
 				etiqueta = document.createElement("label");
-				etiqueta.for = "textarea";
+				etiqueta.for = "textarea1";
 				etiqueta.innerHTML = "<br />Comentarios";
 				formNvo.appendChild(etiqueta);
 
  				campo =document.createElement("textarea");	
- 				campo.id = "textarea";
+ 				campo.id = "textarea1";
 				campo.rows = "5";
 				campo.cols = "50";
 				campo.style.marginLeft="87px";
 				formNvo.appendChild(campo);
 				
 				enviar = document.createElement("button"); 
-  				enviar.type = "button"; 
+  				enviar.type = "submit"; 
   				enviar.innerHTML = "enviar comentario";
   				enviar.onclick = function () {actualizar(opc)}      			
   				formNvo.appendChild(enviar); 
@@ -46,22 +46,22 @@ function rechazado(opc){
   				formNvo.appendChild(evitar); 	
   			break;
   		case 2: formNvo = document.createElement("form");
-				formNvo.id = "formrechazar"; 
+				formNvo.id = "formrechazar2"; 
   				infocotorreo.appendChild(formNvo); 
 				etiqueta = document.createElement("label");
-				etiqueta.for = "textarea";
+				etiqueta.for = "textarea2";
 				etiqueta.innerHTML = "<br />Comentarios";
 				formNvo.appendChild(etiqueta);
  				
  				campo =document.createElement("textarea");	
-	 			campo.id = "textarea";
+	 			campo.id = "textarea2";
 				campo.rows = "5";
 				campo.cols = "50";
 				campo.style.marginLeft="87px";
 				formNvo.appendChild(campo);
 			
 				enviar = document.createElement("button"); 
-  				enviar.type = "button"; 
+  				enviar.type = "submit"; 
   				enviar.innerHTML = "enviar comentario";   
 				enviar.onclick = function () {actualizar(opc)}      			
   				formNvo.appendChild(enviar); 
@@ -72,22 +72,22 @@ function rechazado(opc){
   				formNvo.appendChild(evitar); 	
   			break;
 		case 3: formNvo = document.createElement("form");
-				formNvo.id = "formrechazar"; 
+				formNvo.id = "formrechazar3"; 
   				infoparty.appendChild(formNvo); 
 				etiqueta = document.createElement("label");
-				etiqueta.for = "textarea";
+				etiqueta.for = "textarea3";
 				etiqueta.innerHTML = "<br />Comentarios";
 				formNvo.appendChild(etiqueta);
  				
  				campo =document.createElement("textarea");	
-	 			campo.id = "textarea";
+	 			campo.id = "textarea3";
 				campo.rows = "5";
 				campo.cols = "50";
 				campo.style.marginLeft="87px";
 				formNvo.appendChild(campo);
 			
 				enviar = document.createElement("button"); 
-  				enviar.type = "button"; 
+  				enviar.type = "submit"; 
   				enviar.innerHTML = "enviar comentario";
   				enviar.onclick = function () {actualizar(opc)}      			
   				formNvo.appendChild(enviar); 
@@ -100,7 +100,6 @@ function rechazado(opc){
 	}
 }
 
-
 function actualizar(opc){
 	switch (opc){
 		case 1: eliminar = document.getElementById("lshackers");
@@ -109,7 +108,7 @@ function actualizar(opc){
 				eliminar2.parentNode.removeChild(eliminar2);
 				eliminar3 = document.getElementById("infohackers");
 				eliminar3.parentNode.removeChild(eliminar3);
-				eliminar4 = document.getElementById("formrechazar");
+				eliminar4 = document.getElementById("formrechazar1");
 				eliminar4.parentNode.removeChild(eliminar4);
 			break;	
 		case 2: eliminar = document.getElementById("lscotorreo");
@@ -118,7 +117,7 @@ function actualizar(opc){
 				eliminar2.parentNode.removeChild(eliminar2);
 				eliminar3 = document.getElementById("infocotorreo");
 				eliminar3.parentNode.removeChild(eliminar3);
-				eliminar4 = document.getElementById("formrechazar");
+				eliminar4 = document.getElementById("formrechazar2");
 				eliminar4.parentNode.removeChild(eliminar4);
 			break;
 		case 3: eliminar = document.getElementById("lsparty");
@@ -127,12 +126,47 @@ function actualizar(opc){
 				eliminar2.parentNode.removeChild(eliminar2);
 				eliminar3 = document.getElementById("infoparty");
 				eliminar3.parentNode.removeChild(eliminar3);
-				eliminar4 = document.getElementById("formrechazar");
+				eliminar4 = document.getElementById("formrechazar3");
 				eliminar4.parentNode.removeChild(eliminar4);
 			break;
 	}	
 }
+/*
+enviarComent(opc){
+	switch(opc){
+		case 1: areaTexto= document.getElementById("textarea");
+			if(areaTexto.length == 0 || /^\s+$/.test(areaTexto)){
+				document.formrechazar1.submit();
+				actualizar(opc);	
+			} 
+			else{
+				areaTexto.value = "Necesita escribir un comentario";
+			}
+			break;
+		case 2: areaTexto= document.getElementById("textarea");
+			if(areaTexto.length == 0 || /^\s+$/.test(areaTexto)){
+				document.formrechazar2.submit();
+				actualizar(opc);	
+			} 
+			else{
+				areaTexto.value = "Necesita escribir un comentario";
+			}
+			break;
+		case 3: areaTexto= document.getElementById("textarea");
+			if(areaTexto.length == 0 || /^\s+$/.test(areaTexto)){
+				document.formrechazar3.submit();
+				actualizar(opc);	
+			} 
+			else{
+				areaTexto.value = "Necesita escribir un comentario";
+			}
+			break;
+				
+				
+		
+	}
+}*/
 
 function admModificar(opc){
-	window.open("admModificar.html","_self");
+	window.open("edita.html","_self");
 }
